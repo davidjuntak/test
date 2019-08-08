a3b81aab12d26218071d20aba11d876b15e9a5c2
David Michael Simanjuntak <david.juntak11@gmail.com>
2019-08-08 10:23:34 +0700

    init

---
+++ math_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+    "testing"
+    "reflect"
+)
+
+func TestAdd(t *testing.T) {
+    tests := []struct {
+        a    int
+        b    int
+        expected int
+    }{
+        {
+            a: 1,
+            b: 2,
+            expected: 3,
+        },
+        {
+            a: 3,
+            b: 2,
+            expected: 5,
+        },
+    }
+
+    for _, test := range tests {
+        result := Add(test.a, test.b)
+        if !reflect.DeepEqual(result, test.expected) {
+            t.Errorf("Test failed, expected: %d, result: %d", test.expected, result)
+        }
+    }
+}
