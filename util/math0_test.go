a9a6861c2592b7349d82d795b3c9d33c00a5dd52
David Michael Simanjuntak <david.juntak11@gmail.com>
2019-08-27 14:23:05 +0700

    Update unit test

--- util/math0_test.go
+++ util/math0_test.go
@@ -110,28 +110,3 @@ func TestAdd0_3(t *testing.T) {
     }
 }
 
-func TestAdd0_4(t *testing.T) {
-    tests := []struct {
-        a    int
-        b    int
-        expected int
-    }{
-        {
-            a: 1,
-            b: 2,
-            expected: 3,
-        },
-        {
-            a: 3,
-            b: 2,
-            expected: 5,
-        },
-    }
-
-    for _, test := range tests {
-        result := Add0_4(test.a, test.b)
-        if !reflect.DeepEqual(result, test.expected) {
-            t.Errorf("Test failed")
-        }
-    }
-}
