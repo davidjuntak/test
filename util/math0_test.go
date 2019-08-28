2eeb16fbfcad5910a42e338e5695b6c82c7c3169
David Michael Simanjuntak <david.juntak11@gmail.com>
2019-08-28 14:38:07 +0700

    Remove unit test

--- util/math0_test.go
+++ util/math0_test.go
@@ -84,29 +84,3 @@ func TestAdd0_2(t *testing.T) {
     }
 }
 
-func TestAdd0_3(t *testing.T) {
-    tests := []struct {
-        a    int
-        b    int
-        expected int
-    }{
-        {
-            a: 1,
-            b: 2,
-            expected: 3,
-        },
-        {
-            a: 3,
-            b: 2,
-            expected: 5,
-        },
-    }
-
-    for _, test := range tests {
-        result := Add0_3(test.a, test.b)
-        if !reflect.DeepEqual(result, test.expected) {
-            t.Errorf("Test failed")
-        }
-    }
-}
-
